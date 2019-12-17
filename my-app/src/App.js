a3cf4e8b0d2e73d3bd8d5404c712ca5e4b86f56b
muktidj <muktidwijatmoko@gmail.com>
2019-12-17 23:17:15 +0700

    Bab 3 3.Membuat folder && file Person.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,18 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
+import Person from './Person/Person';
 
-function App() {
-  // return (
-  //   <div className="App">
-  //     <h1>Hi, I'am learn React</h1>
-  //   </div>
-  // );
-
-  // React Without JSX in Background 
-  return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does is it Work ?'))
+class App extends Component {
+  render() {
+  return (
+    <div className="App">
+      <h1>Hi, I'am learn React</h1>
+      <Person />
+    </div>
+  );
+  }
+  // React Without JSX in Background
+  // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does is it Work ?'))
 }
 
 export default App;
---
+++ my-app/src/Person/Person.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const person = () => {
+  return <p>I'am Ghost Man</p>
+};
+
+export default person;
