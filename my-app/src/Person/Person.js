1809b93e15995629efda09d8cf30b692bb1035e2
muktidj <muktidwijatmoko@gmail.com>
2019-12-17 23:41:34 +0700

    Bab 3 4. Bekerja dengan dynamic content pada person.js kemudian ditautkan di app.js

--- my-app/src/Person/Person.js
+++ my-app/src/Person/Person.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const person = () => {
-  return <p>I'am Ghost Man</p>
+  return <p>I'am Ghost Man, I'am {Math.floor(Math.random() * 30)} years old</p>
 };
 
 export default person;
