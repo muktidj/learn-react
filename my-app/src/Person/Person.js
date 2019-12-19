9a029d674d7e4b768946545366650fd4d60be624
muktidj <muktidwijatmoko@gmail.com>
2019-12-19 15:44:53 +0700

    Bab 3 6. Memahami Properti Children pada Person.js

--- my-app/src/Person/Person.js
+++ my-app/src/Person/Person.js
@@ -1,7 +1,14 @@
 import React from "react";
 
-const person = (props) => {
-  return <p>I'am {props.name}, I'am {props.age} years old</p>
+const person = props => {
+  return (
+    <div>
+      <p>
+        I'am {props.name}, I'am {props.age} years old
+      </p>
+      <p>{props.children}</p>
+    </div>
+  )
 };
 
 export default person;
