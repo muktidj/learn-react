ebccb15ba7dc87b1f3458578516eedf126c4abd3
muktidj <muktidwijatmoko@gmail.com>
2020-01-06 16:32:20 +0700

    Bab 5 5.Menginstall styled-component dan menggunakannya diperson.js

--- my-app/src/Person/Person.js
+++ my-app/src/Person/Person.js
@@ -1,22 +1,32 @@
 import React from "react";
-import Radium from "radium";
-import './Person.css'
+import styled from 'styled-components'
+//import './Person.css'
+
+const StyledDiv = styled.div`
+  width: 60%;
+  margin: 13px auto;
+  text-align: center;
+  border: 1px solid #eee;
+  box-shadow: 0 2px 2px #ccc;
+  padding: 16px;
+
+  @media (min-width:500px) {
+    width: '450px'
+  }
+
+`
 
 const person = props => {
-  const stylePerson = {
-    '@media (min-width:500px)': {
-      width: '450px'
-    }
-  }
+
   return (
-    <div className="Person" style={stylePerson}>
+    <StyledDiv>
       <p onClick={props.click}>
         I'am {props.name}, I'am {props.age} years old
       </p>
       <p>{props.children}</p>
       <input name="text" onChange={props.changed} value={props.name} />
-    </div>
+    </StyledDiv>
   );
 };
 
-export default Radium(person);
+export default person;
